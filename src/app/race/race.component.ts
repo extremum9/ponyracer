362e660fb0ef0282925b0c9985b9056cec95e3e2
denyschr <denprogman99@gmail.com>
2024-07-16 23:09:18 +0300

    feat: show instant start date of each race

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RaceModel } from '../models/race.model';
 import { PonyComponent } from '../pony/pony.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'pr-race',
   standalone: true,
-  imports: [PonyComponent],
+  imports: [PonyComponent, DatePipe],
   templateUrl: './race.component.html',
   styleUrl: './race.component.css'
 })
