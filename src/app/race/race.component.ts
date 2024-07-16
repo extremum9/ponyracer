73622ba1fb301bc775d06145ac46e2641daf97d1
denyschr <denprogman99@gmail.com>
2024-07-16 23:38:00 +0300

    feat: use date-fns to display pretty dates

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RaceModel } from '../models/race.model';
 import { PonyComponent } from '../pony/pony.component';
-import { DatePipe } from '@angular/common';
+import { FromNowPipe } from '../from-now.pipe';
 
 @Component({
   selector: 'pr-race',
   standalone: true,
-  imports: [PonyComponent, DatePipe],
+  imports: [PonyComponent, FromNowPipe],
   templateUrl: './race.component.html',
   styleUrl: './race.component.css'
 })
